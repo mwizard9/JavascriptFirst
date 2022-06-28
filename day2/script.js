185492f3ea81586eb95dc4f995ae5ae4231171af
mwizard9 <manishhimalaya64@gmail.com>
2022-06-28 21:37:25 +0545

    varfun

--- day2/script.js
+++ day2/script.js
@@ -2,4 +2,4 @@ function toCelsius(f){
     return (5/9) * (f-32);
 }
 document.getElementById("celsius").innerHTML=toCelsius(132);
-document.getElementById("celsius").innerHTML="the tempreture is" +toCelsius(77) + "celsius";
+document.getElementById("celsius").innerHTML="the tempreture is " + toCelsius(77) + " celsius";
