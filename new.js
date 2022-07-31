e4690dc52912d85f1a58558782c6cbe3a1cffe99
mwizard9 <manishhimalaya64@gmail.com>
2022-07-31 18:00:17 +0545

    quotes under quotes

--- new.js
+++ new.js
@@ -5,3 +5,9 @@ function displayDate(){
 let fname = "Manish";
 let lname = 'Himalaya';
 document.getElementById('name').innerHTML = fname + " " + lname ;
+
+let answer1 = "My name is 'Manish Himalaya'";
+let answer2 = 'i live in "madhyapur thimi"';
+let answer3 = "my brother's name is 'anish HImalaya'";
+document.getElementById('statement').innerHTML = 
+answer1 + "<br>" +answer2 + "<br>" + answer3
