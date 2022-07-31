45368a1e8f3fd77f1599332b2e65e399da9391c6
mwizard9 <manishhimalaya64@gmail.com>
2022-07-31 18:08:24 +0545

    object & string

--- new.js
+++ new.js
@@ -10,4 +10,8 @@ let answer1 = "My name is 'Manish Himalaya'";
 let answer2 = 'i live in "madhyapur thimi"';
 let answer3 = "my brother's name is 'anish HImalaya'";
 document.getElementById('statement').innerHTML = 
-answer1 + "<br>" +answer2 + "<br>" + answer3
+answer1 + "<br>" +answer2 + "<br>" + answer3 ;
+
+let x = "manish himalaya"; //x is astring
+let y= new String("manish himalaya"); //y is an object
+document.getElementById("object").innerHTML = (x==y);
