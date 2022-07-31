6834fea9e0717e527c9f17b26b15e60d201e15d5
mwizard9 <manishhimalaya64@gmail.com>
2022-07-31 17:53:07 +0545

    string implementation

--- new.js
+++ new.js
@@ -1,3 +1,7 @@
 function displayDate(){
     document.getElementById("date").innerHTML=Date();
 }
+
+let fname = "Manish";
+let lname = 'Himalaya';
+document.getElementById('name').innerHTML = fname + " " + lname ;
